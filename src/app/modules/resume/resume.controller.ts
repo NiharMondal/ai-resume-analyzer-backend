97a4d9c93d73ca2d6aa3e24415a4a620b3ce4a92
Nihar Mondal <developernihar@gmail.com>
2025-01-10 01:39:35 +0600

    feat: resume upload and set file destinaiton

--- src/app/modules/resume/resume.controller.ts
+++ src/app/modules/resume/resume.controller.ts
@@ -1,3 +1,16 @@
-const createIntoDB = async () => {};
+import { Request, Response } from "express";
+import handleAsync from "../../../utils/handleAsync";
+import { resumeServices } from "./resume.service";
+import sendResponse from "../../../utils/sendResponse";
+
+const createIntoDB = handleAsync(async (req: Request, res: Response) => {
+	const result = await resumeServices.createIntoDB(req.file);
+
+	sendResponse(res, {
+		statusCode: 201,
+		message: "Resume uploaded successfully",
+		result: result,
+	});
+});
 
 export const resumeControllers = { createIntoDB };
--- src/routes/routesArr.ts
+++ src/routes/routesArr.ts
@@ -2,6 +2,6 @@ import { resumeRouter } from "../app/modules/resume/resume.routes";
 import { userRouter } from "../app/modules/user/user.routes";
 
 export const routesArr = [
-	{ path: "/resume", contl: resumeRouter },
+	{ path: "/upload-resume", contl: resumeRouter },
 	{ path: "/user", contl: userRouter },
 ];
